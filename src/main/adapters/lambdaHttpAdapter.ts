2d978a258ff1e8ecf20b674763dbed8ae81d6038
Birgiman <eduardo.birgiman@outlook.com>
2025-07-28 18:56:37 -0300

    feat(/erros): Creating custom error handler

--- src/main/adapters/lambdaHttpAdapter.ts
+++ src/main/adapters/lambdaHttpAdapter.ts
@@ -1,7 +1,10 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
 import { ZodError } from 'zod';
 import { Controller } from '../../application/contracts/Controller';
+import { ErrorCode } from '../../application/errors/ErrorCode';
+import { HttpError } from '../../application/errors/http/HttpError';
 import { lambdaBodyParser } from '../utils/lambdaBodyParser';
+import { lambdaErrorResponse } from '../utils/lambdaErrorResponse';
 
 export function lambdaHttpAdapter(controller: Controller<unknown>) {
   return async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
@@ -21,28 +24,25 @@ export function lambdaHttpAdapter(controller: Controller<unknown>) {
       }
     } catch (error) {
       if (error instanceof ZodError) {
-        return {
+        return lambdaErrorResponse({
           statusCode: 400,
-          body: JSON.stringify({
-            error: {
-              code: 'VALIDATION',
-              message: error.issues.map(issue => ({
-                field: issue.path.join('.'), //acount.name
-                error: issue.message
-              })),
-            }
-          })
-        }
-      }
-      return {
-        statusCode: 500,
-        body: JSON.stringify({
-          error: {
-            code: 'INTERNAL_SERVER_ERROR',
-            message: 'Internal server error',
-          }
+          code: ErrorCode.VALIDATION,
+          message: error.issues.map(issue => ({
+            field: issue.path.join('.'), //acount.name
+            error: issue.message
+          })),
         })
       }
+
+      if (error instanceof HttpError) {
+        return lambdaErrorResponse(error);
+      }
+
+      return lambdaErrorResponse({
+        statusCode: 500,
+        code: ErrorCode.INTERNAL_SERVER_ERROR,
+        message: 'Internal server error',
+      })
     }
   }
 }
