29f524ec157de6ebee11398ed79efe8c9679e0f1
Birgiman <eduardo.birgiman@outlook.com>
2025-07-28 00:10:15 -0300

    chore: Installing ZOD library for types

--- src/application/controllers/HelloController.ts
+++ src/application/controllers/HelloController.ts
@@ -1,10 +1,19 @@
+import { z } from 'zod';
 import { IController } from '../contracts/Controller';
+
+const schema = z.object({
+  name: z.string().min(1, 'Name is required'),
+  email: z.email('Invalid email').min(1, 'E-mail is required'),
+})
 
 export class HelloController implements IController<unknown> {
   async handle(request: IController.Request): Promise<IController.Response<unknown>> {
+
+    const parsedBody = schema.safeParse(request.body)
+
     return {
       statusCode: 200,
-      body: { request }
+      body: { parsedBody }
     }
   }
 }
