74bd2c607f917df76f36d33900b7691cd3a94dc2
Birgiman <eduardo.birgiman@outlook.com>
2025-07-27 23:33:55 -0300

    feat: Creating lambdaHttpAdapter to abstract and refactor hello handler and refactor Controller.ts

--- src/main/adapters/lambdaHttpAdapter.ts
+++ src/main/adapters/lambdaHttpAdapter.ts
@@ -1,16 +1,21 @@
-import { APIGatewayProxyEventV2 } from 'aws-lambda';
+import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
+import { IController } from '../../application/contracts/Controller';
 import { lambdaBodyParser } from '../utils/lambdaBodyParser';
 
-export function lambdaHttpAdapter(controller: any) {
-  return async (event: APIGatewayProxyEventV2) => {
+export function lambdaHttpAdapter(controller: IController<unknown>) {
+  return async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
     const body = lambdaBodyParser(event.body);
     const params = event.pathParameters ?? {}
 
     const queryParams = event.queryStringParameters ?? {}
-    return controller.handle({
+    const response = await controller.handle({
       body,
       params,
       queryParams,
     });
+    return {
+      statusCode: response.statusCode,
+      body: response.body ? JSON.stringify(response.body) : undefined,
+    }
   }
 }
